d878367fac1d61c4ff3181cf5d09ad0ebc181b66
vincenziok-dot <vincenziok@gmail.com>
2025-12-07 20:21:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('placeholder');
+var map = new maplibregl.Map({
+    container:'map',
+    style:{
+      version:8,
+      sources:{
+        'empty':{ type:'raster', tiles:[], tileSize:256 }
+      },
+      layers:[
+        { id:'background', type:'background', paint:{ 'background-color':'#000' } }
+      ]
+    },
+    center:[10.99,45.44],
+    zoom:9
+});
+
+map.on('load', ()=>{ console.log("Map loaded placeholder"); });
