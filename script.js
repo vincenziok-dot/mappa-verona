fef0d7d5b11292e90dd49a1f94d3cedf0318e77f
vincenziok-dot <vincenziok@gmail.com>
2025-12-07 20:46:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-var map = new maplibregl.Map({
-    container:'map',
-    style:{
-      version:8,
-      sources:{
-        'empty':{ type:'raster', tiles:[], tileSize:256 }
-      },
-      layers:[
-        { id:'background', type:'background', paint:{ 'background-color':'#000' } }
-      ]
-    },
-    center:[10.99,45.44],
-    zoom:9
+const map=new maplibregl.Map({
+container:'map',
+style:{version:8,sources:{},layers:[{id:'bg',type:'background',paint:{'background-color':'#000'}}]},
+center:[10.99,45.44],
+zoom:8,
+pitch:45,
+bearing:-20
 });
 
-map.on('load', ()=>{ console.log("Map loaded placeholder"); });
+map.on('load',()=>{
+ map.addSource('comuni',{type:'geojson',data:'data/comuni_verona.geojson'});
+ map.addLayer({
+  id:'comuni-fill',
+  type:'fill-extrusion',
+  source:'comuni',
+  paint:{
+   'fill-extrusion-color':'#003322',
+   'fill-extrusion-height':2000,
+   'fill-extrusion-opacity':0.3
+  }
+ });
+ map.addLayer({
+  id:'comuni-line',
+  type:'line',
+  source:'comuni',
+  paint:{'line-color':'#00ff9c','line-width':1.5}
+ });
+});
