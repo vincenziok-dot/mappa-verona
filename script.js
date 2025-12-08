898e5c1959fba2b7573a88992ed612e7a1a05e27
vincenziok-dot <vincenziok@gmail.com>
2025-12-08 23:33:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const map = new maplibregl.Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/style.json',
-  center: [10.99, 45.44],
-  zoom: 9,
+
+  // CENTRO SU VERONA
+  center: [10.9916, 45.4384],
+  zoom: 10,
   pitch: 45,
-  bearing: -20
+  bearing: -15,
+
+  // BLOCCA LA MAPPA SU VERONA (niente giro per l’Europa)
+  maxBounds: [
+    [10.85, 45.33], // sud-ovest
+    [11.15, 45.55]  // nord-est
+  ]
 });
 
 map.on('load', () => {
@@ -13,24 +21,33 @@ map.on('load', () => {
     data: 'data/comuni_verona_metropolitana.geojson'
   });
 
+  // RIEMPIMENTO (futuristico, leggero)
   map.addLayer({
     id: 'comuni-fill',
     type: 'fill-extrusion',
     source: 'comuni',
     paint: {
-      'fill-extrusion-color': '#003322',
-      'fill-extrusion-height': 2000,
+      'fill-extrusion-color': '#004433',
+      'fill-extrusion-height': 1500,
       'fill-extrusion-opacity': 0.35
     }
   });
 
+  // CONTORNI VERDI COME DA UMAPP
   map.addLayer({
     id: 'comuni-line',
     type: 'line',
     source: 'comuni',
     paint: {
       'line-color': '#00ff9c',
-      'line-width': 1.8
+      'line-width': 1.5
     }
   });
+
+  // 👉 ZOOM AUTOMATICO SUI COMUNI
+  const bounds = new maplibregl.LngLatBounds();
+  comuni.features.forEach(f => {
+    f.geometry.coordinates[0].forEach(c => bounds.extend(c));
+  });
+  map.fitBounds(bounds, { padding: 40, duration: 1000 });
 });
