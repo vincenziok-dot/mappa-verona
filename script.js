797f98e229682a98486f63180e210c9dc26b97a0
vincenziok-dot <vincenziok@gmail.com>
2025-12-09 00:25:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const map = L.map('map').setView([45.4384, 10.9916], 12);
-
+layer.setStyle({
+  color: '#00ff9c',
+  weight: 2,
+  fillOpacity: 0
+});
 L.tileLayer(
   'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
   {
