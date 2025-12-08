c941177b61a2f32252dadbbdd1cf9ee2f75eaa8e
vincenziok-dot <vincenziok@gmail.com>
2025-12-08 23:48:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.body.style.background = 'red';
-alert('JS CARICATO');
+const map = L.map('map').setView([45.4384, 10.9916], 12);
+
+L.tileLayer(
+  'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+  {
+    attribution: '© OpenStreetMap, © CARTO',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }
+).addTo(map);
